8e3b5b80ed6a95f315e30e9d700024c13e057720
alphablond <gabypensador3@gmail.com>
2026-02-27 07:50:42 +0000

    Delete api directory

--- api/v1/status.js
+++
@@ -1,9 +0,0 @@
-import { success } from "../utils/response.js";
-
-export default function handler(req, res) {
-  return success(res, {
-    service: "Dominexa API",
-    version: "v1",
-    status: "operational"
-  });
-}
